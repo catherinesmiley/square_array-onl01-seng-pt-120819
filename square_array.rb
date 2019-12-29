5da34c9a8ab6a9171978c0f2d51236d28aae046f
Catherine Smiley <catherinesmiley@gmail.com>
2019-12-29 20:47:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -23,10 +23,10 @@
 
 
 
-def square_array(numbers)
-  new_array = []
-  numbers.each do |number|
-    new_array << number ** 2 
-  end
-  new_array
-end
+# def square_array(numbers)
+#   new_array = []
+#   numbers.each do |number|
+#     new_array << number ** 2 
+#   end
+#   new_array
+# end
